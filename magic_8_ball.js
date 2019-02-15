5149c99c09dfc0e2cda586d31b20d5493ac10634
brian mosesso <bmosesso@gmail.com>
2019-02-14 20:49:15 -0700

    magic 8 ball

---
+++ magic_8_ball.js
@@ -0,0 +1,44 @@
+let userName = '';
+
+if (userName) {
+    console.log(`Hello, ${userName}`);
+} else {
+    console.log('Hello!');
+}
+
+let userQuestion = 'Will I get a raise?';
+
+console.log(`${userQuestion}`);
+
+let randomNumber = Math.floor(Math.random() * 8);
+
+// console.log(`${randomNumber}`)
+
+let eightBall = '';
+
+switch (randomNumber) {
+  case 0:
+    console.log('It is certain');
+    break;
+  case 1:
+    console.log('It is decidedly so');
+    break;
+  case 2:
+    console.log('Reply hazy try again');
+    break;
+  case 3:
+    console.log('Cannot predict now');
+    break;
+  case 4:
+    console.log('Do not count on it');
+    break;
+  case 5:
+    console.log('My sources say no');
+    break;
+  case 6:
+    console.log('Outlook not so good');
+    break;
+  case 7:
+    console.log('Signs point to yes');
+    break;
+}
